a13aa59a0e920d439da68f70645483f56f2ab7c0
gonzalez.yasir <gonzalez.yasir@SEMS-259-11.SEMS259.LAB>
2018-03-14 14:56:21 -0700

    PiDay

---
+++ src/PiDay.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class PiDay {
+	public static void main(String[] args) {
+String Pi = "3.1415926535" ;
+for (int i = 0; i <4; i++) {
+	System.out.print(Pi.charAt(i));
+}
+for (int i = 4; i <Pi.length(); i++) {
+String o =Character.toString(Pi.charAt(i));
+	String a=JOptionPane.showInputDialog("what is the next digit of pie?");
+ if(a.equals(o)){
+	JOptionPane.showMessageDialog(null, "Correct");
+	
+ }else {
+
+	 JOptionPane.showMessageDialog(null, "Incorrrect, NO PIE FOR YOU!!!!");
+ }
+}
+}
